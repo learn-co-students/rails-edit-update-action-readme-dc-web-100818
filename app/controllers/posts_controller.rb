7a81df6e1075681acb3f1b9d6dc176b95528fe7b
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-11-05 17:31:27 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,38 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
+  def show
+  end
+
   def create
-    @post = Post.new
-    @post.title = params[:title]
-    @post.description = params[:description]
-    @post.save
+    @post = Post.create(post_params)
     redirect_to post_path(@post)
   end
 
   # add edit and update methods here
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-  @post = Post.find(params[:id])
-  @post.update(title: params[:post][:title], description: params[:post][:description])
+  @post.update(post_params)
   redirect_to post_path(@post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
